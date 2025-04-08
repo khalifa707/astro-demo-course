631e75dd20fd58615c3db7dfee47b45ea7210cfc
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-04-08 10:53:54 +0200

    Working with Content Collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,8 +1,7 @@
 // 1. Import utilities from `astro:content`
 import { defineCollection, z } from 'astro:content';
 
-// 2. Import loader(s)
-import { glob, file } from 'astro/loaders';
+
 
 // 3. Define your collection(s)
 const postsCollection = defineCollection({ /* ... */ });
