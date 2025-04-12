38a56762aad401377b43968f50e649ff8c9ffa07
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-04-13 00:48:06 +0200

    Working with Content Collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,19 +1,17 @@
-// 1. Import utilities from `astro:content`
 import { defineCollection, z } from 'astro:content';
-import {format} from 'date-fns';
-
-
+import { format } from 'date-fns';
 
 // 3. Define your collection(s)
 const postsCollection = defineCollection({
-    schema:z.object({
-        author:z.string(),
-        categories:z.array(z.string()),
-        title:z.string(),
-        date:z.string().transform(str=>format(new Date(str), 'MM-dd-yyyy')),
-        featured:z.boolean().default(false),
+    schema: z.object({
+        author: z.string(),
+        categories: z.array(z.string()),
+        title: z.string(),
+        date: z.string().transform(str => format(new Date(str), 'MM-dd-yyyy')),
+        featured: z.boolean().default(false),
+        image: z.string(), // This should be just the filename or path as a string
     }),
-  });
+});
 
 // 4. Export a single `collections` object to register your collection(s)
 export const collections = {
